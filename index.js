0f4074e9a088c08d627826154cde74ccd51e8a5c
hani.ismail <haniismail@takiacademyteam.com>
2024-11-05 12:17:54 +0100

    minor change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-  'mongodb+srv://amine:amine@cluster0.exa0s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://task-manager:1234Five@task-manager.tllrg2b.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
